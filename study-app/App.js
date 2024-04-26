bbd6c1d98fec370cf32808923a9d20f94e8d4735
Isthefani Vitoria <89088930+isthefani@users.noreply.github.com>
2024-04-25 21:00:26 -0300

    new files

---
+++ study-app/App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StudyCardsProvider } from './src/contexts/StudyCardsContext';
+import CardListScreen from './src/screens/CardListScreen';
+import CardEditScreen from './src/screens/CardEditScreen';
+
+const Stack = createStackNavigator();
+
+const App = () => {
+    return (
+        <StudyCardsProvider>
+            <NavigationContainer>
+                <Stack.Navigator initialRouteName="CardList">
+                    <Stack.Screen name="CardList" component={CardListScreen} options={{ title: 'Study Cards' }} />
+                    <Stack.Screen name="CardEdit" component={CardEditScreen} options={{ title: 'Editar Card' }} />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </StudyCardsProvider>
+    );
+};
+
+export default App;
